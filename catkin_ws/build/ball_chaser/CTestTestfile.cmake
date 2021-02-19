50f524a0befcfffe1aa27c88326ecd31153c122f
José Luis Pascal <jose.pascal@gmail.com>
2021-02-19 19:06:43 -0300

    Delete catkin_ws/build directory

--- catkin_ws/build/ball_chaser/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/argosx/Documents/codes/robotics-software-engineer-udacity/3_ROS_Essentials/project_go_chase_it/catkin_ws/src/ball_chaser
-# Build directory: /home/argosx/Documents/codes/robotics-software-engineer-udacity/3_ROS_Essentials/project_go_chase_it/catkin_ws/build/ball_chaser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
